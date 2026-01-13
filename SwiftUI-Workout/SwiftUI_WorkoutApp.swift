395a6ce8196f6d1a5c0ffcba95633fb3ecc3bba9
ShreyasTantri <shreyastantrik29@gmail.com>
2026-01-13 09:02:57 +0530

    Initial Commit

---
+++ SwiftUI-Workout/SwiftUI_WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WorkoutApp.swift
+//  SwiftUI-Workout
+//
+//  Created by CCS038 on 13/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
